f44c0ccc207c2b5ea66577cf17446b01e1855a09
Monster <coolpest@gmail.com>
2020-06-20 16:41:21 +0800

    代码提交

--- Chapter1/main.go
+++ Chapter1/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type gUser struct {
+	ID uint64 `json:"id"`
+	Name string `json:"name"`
+	WeChat string `json:"weChat"`
+}
 
 func main() {
-	fmt.Println("c1")
+	//users:=[]User{{ID:123,Name:"张三丰"},{ID:456,Name:"张无忌"}}
+	r:=gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK,gin.H{
+			"name":"小红",
+			"desc":"漂亮的小姐姐，我来了，今天是520，你有人约会吗？",
+		})
+	})
+	//http://127.0.0.1:8889/123?name=老王
+	r.GET("/users/:id", func(context *gin.Context) {
+		id:=context.Param("id")
+		fmt.Println("我的Id 是"+id)
+		name:=context.Query("name")
+
+		context.JSON(http.StatusOK,gin.H{
+			"name":name,
+			"desc":"小姐姐，我的 ID是"+id,
+
+		})
+	})
+	r.GET("/slice", func(context *gin.Context) {
+		context.JSON(http.StatusOK,context.QueryArray("media"))
+	})
+	r.GET("/map", func(context *gin.Context) {
+		context.JSON(http.StatusOK,context.QueryMap("ids"))
+	})
+	r.POST("/users", func(context *gin.Context) {
+		//创建一个新用户
+		var user User
+		err := context.BindJSON(&user)
+		if err != nil {
+			panic(err)
+		}
+		context.JSON(http.StatusOK,gin.H{"user":user})
+
+	})
+	r.GET("/users", func(context *gin.Context) {
+		//获取全部用户
+	})
+	r.PUT("/user/:id", func(context *gin.Context) {
+		//更新用户id 为 xx 的信息
+	})
+	r.DELETE("/user/:id", func(context *gin.Context) {
+		//删除用户 id 为 xx
+	})
+	r.Run(":8889")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/i-coder-robot/gin-demo
 
 go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b // indirect
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
